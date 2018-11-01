94d7670e2ea2ea4de905e4c1766850c21635c387
Vic-L <vljc17@gmail.com>
2018-11-01 22:34:52 +0800

    feat: twitter widget js

---
+++ twitter.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const axios = require('axios')
+const path = require('path')
+const PingSlack = require('./services/pingSlack').pingSlack
+const aws = require('aws-sdk')
+
+module.exports.call = async (event, context) => {
+  // TODO validate twitterWidgetJsPath does not start with /
+
+  //  define paths here so that can unlink file in catch block
+  const twitterWidgetPath = path.resolve(__dirname, 'tmp', 'twitterWidget.js')
+
+  try {
+    // s3 setup
+    const s3 = new aws.S3({
+      accessKeyId: process.env.s3AccessKeyId,
+      secretAccessKey: process.env.s3SecretAccessKey,
+      region: process.env.s3Region
+    })
+    const s3Params = {
+      Bucket: process.env.s3Bucket,
+      CacheControl: 'public,max-age=604800',
+      ContentType: 'application/javascript; charset=utf-8',
+      ACL: 'public-read',
+    }
+    ///// twitter widget.js START /////
+    // download pinit_main file
+    const downloadTwitterWidgetResp = await axios({
+      method: 'GET',
+      url: 'https://platform.twitter.com/widgets.js',
+      responseType: 'stream'
+    })
+
+    // stream into s3
+    s3Params['Key'] = process.env.twitterWidgetJsPath
+    s3Params['Body'] = downloadTwitterWidgetResp.data
+    await s3.upload(s3Params).promise()
+    ///// twitter widget.js END /////
+
+    const attachment = {}
+    attachment['text'] = 'Twitter widget vendor files uploaded successfully'
+    attachment['title'] = 'SUCCESS - Pinterest vendor files'
+    attachment['color'] = 'good'
+    await PingSlack({
+      slackUrl: process.env.slackUrl,
+      attachment: attachment,
+    })
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Successfully pinterest files uploaded to S3'
+      }),
+    }
+  } catch (err) {
+    const attachment = {}
+    attachment['text'] = `Error when uploading Pinterest vendor files\Message: ${err.message}`
+    attachment['title'] = 'FAILURE - Pinterest vendor files'
+    attachment['color'] = 'danger'
+    await PingSlack({
+      slackUrl: process.env.slackUrl,
+      attachment: attachment,
+      channel: process.env.slackChannel,
+    })
+
+    console.log('err', err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err.message
+      }),
+    }
+  }
+}
