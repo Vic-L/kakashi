8c49e81af15209ed1edcf6c9e782bbff6eeee9c8
Vic-L <vljc17@gmail.com>
2018-11-01 20:41:11 +0800

    feat: download, modify and delete piniterest vendor file

--- pinterest.js
+++ pinterest.js
@@ -1,20 +1,48 @@
 'use strict'
 
-const pingSlack = require('./services/pingSlack').pingSlack
+const axios = require('axios')
+const fs = require('fs')
+const path = require('path')
+const replace = require('replace-in-file')
+const PingSlack = require('./services/pingSlack').pingSlack
 
 module.exports.call = async (event, context) => {
   try {
-    await pingSlack({
-      slackUrl: process.env.slackUrl,
+    // download file
+    const downloadResp = await axios({
+      method: 'GET',
+      url: 'https://assets.pinterest.com/js/pinit.js',
+      responseType: 'stream'
     })
+
+    const filepath = path.resolve(__dirname, 'tmp', 'pinit.js')
+
+    // write to file
+    await downloadResp.data.pipe(fs.createWriteStream(filepath))
+
+    // change file content
+    const options = {
+      files: filepath,
+      from: '//assets.pinterest.com/js/pinit_main.js',
+      to: process.env.pinitMainJsPath,
+    }
+    await replace(options)
+
+    // upload file to S3
+
+    // delete file
+    await fs.unlink(filepath)
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Ping slack successful!'
+        message: 'downloaded and deleted file'
       }),
     }
   } catch (err) {
+    // delete file if fail
+    await fs.unlink(filepath)
+
     console.log('err', err)
     return {
       statusCode: 500,
