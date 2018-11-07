ea607d64a66d6d11488c1f80dbbb52f48be06fb8
Vic-L <vljc17@gmail.com>
2018-11-07 20:40:55 +0900

    feat: facebookSDK

--- facebook.js
+++ facebook.js
@@ -2,18 +2,16 @@
 
 const axios = require('axios')
 const fs = require('fs')
-const path = require('path')
 const replace = require('replace-in-file')
 const PingSlack = require('./services/pingSlack').pingSlack
 const aws = require('aws-sdk')
 
 module.exports.call = async (event, context) => {
-  // TODO validate pinitJsPath does not start with /
-  // TODO validate pinitMainJsPath does not start with /
+  // TODO validate facebookSDKJSPath does not start with /
 
   // define paths here so that can unlink file in catch block
   // lambda support read/write to /tmp only for now
-  const pinitPath = '/tmp/pinit.js'
+  const facebookSDKPath = '/tmp/facebookSDK.js'
 
   try {
     // s3 setup
@@ -28,52 +26,39 @@ module.exports.call = async (event, context) => {
       ContentType: 'application/javascript; charset=utf-8',
       ACL: 'public-read',
     }
-    ///// pinit_main START /////
-    // download pinit_main file
-    const downloadPinitMainResp = await axios({
-      method: 'GET',
-      url: 'https://assets.pinterest.com/js/pinit_main.js',
-      responseType: 'stream'
-    })
 
-    // stream into s3
-    s3Params['Key'] = process.env.pinitMainJsPath
-    s3Params['Body'] = downloadPinitMainResp.data
-    await s3.upload(s3Params).promise()
-    ///// pinit_main END /////
-
-    ///// pinit file START /////
-    // download pinit file
-    const downloadPinitResp = await axios({
+    ///// facebookSDK START /////
+    // download facebook sdk file
+    const downloadFBSDKResp = await axios({
       method: 'GET',
-      url: 'https://assets.pinterest.com/js/pinit.js',
+      url: 'https://connect.facebook.net/en_US/sdk.js',
       responseType: 'stream'
     })
 
     // write to file
-    await downloadPinitResp.data.pipe(fs.createWriteStream(pinitPath))
+    await downloadFBSDKResp.data.pipe(fs.createWriteStream(facebookSDKPath))
 
     // change file content
     const options = {
-      files: pinitPath,
-      from: '//assets.pinterest.com/js/pinit_main.js',
-      to: `//${process.env.assetHostName}/${process.env.pinitMainJsPath}`,
+      files: facebookSDKPath,
+      from: 'https:\/\/connect.facebook.net\/en_US\/sdk.js',
+      to: `https:\/\/${process.env.assetHostName}\/${process.env.facebookSDKJSPath.replace('/', '\/')}`,
     }
     await replace(options)
 
-    // stream file to S3
-    const pinitStream = await fs.createReadStream(pinitPath)
-    s3Params['Key'] = process.env.pinitJsPath
-    s3Params['Body'] = pinitStream
+    // // stream file to S3
+    const FBSDKStream = await fs.createReadStream(facebookSDKPath)
+    s3Params['Key'] = process.env.facebookSDKJSPath
+    s3Params['Body'] = FBSDKStream
     await s3.upload(s3Params).promise()
 
     // delete file
-    await fs.unlink(pinitPath)
-    ///// pinit file END /////
+    // await fs.unlink(facebookSDKPath)
+    ///// facebookSDK file END /////
 
     const attachment = {}
-    attachment['text'] = 'Pinterest vendor files uploaded successfully'
-    attachment['title'] = 'SUCCESS - Pinterest vendor files'
+    attachment['text'] = 'Facebook SDK vendor files uploaded successfully'
+    attachment['title'] = 'SUCCESS - Facebook SDK vendor files'
     attachment['color'] = 'good'
     await PingSlack({
       slackUrl: process.env.slackUrl,
@@ -83,18 +68,18 @@ module.exports.call = async (event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Successfully pinterest files uploaded to S3'
+        message: 'Successfully Facebook SDK files uploaded to S3'
       }),
     }
   } catch (err) {
     // delete files if fail
-    if (pinitPath) {
-      await fs.unlink(pinitPath)
+    if (facebookSDKPath) {
+      await fs.unlink(facebookSDKPath)
     }
 
     const attachment = {}
-    attachment['text'] = `Error when uploading Pinterest vendor files\Message: ${err.message}`
-    attachment['title'] = 'FAILURE - Pinterest vendor files'
+    attachment['text'] = `Error when uploading Facebook SDK vendor files\Message: ${err.message}`
+    attachment['title'] = 'FAILURE - Facebook SDK vendor files'
     attachment['color'] = 'danger'
     await PingSlack({
       slackUrl: process.env.slackUrl,
